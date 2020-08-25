a06f1d0555966608c76ee7dab25e84252cf117b4
benwright <benwright6969@gmail.com>
2020-08-25 01:46:56 +0100

    Reformatted page and added preloading of images

--- js/pongGame.js
+++ js/pongGame.js
@@ -10,7 +10,7 @@ var ballSpeedY = 4;
 //defining scores
 var player1Score = 0;
 var player2Score = 0;
-const WINNING_SCORE = 15;
+const WINNING_SCORE = 5;
 
 var showingWinScreen = false;
 //defining the paddle designs
